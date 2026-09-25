560322a9b73ce2f0d0e931314d9657b92221b154
Kamran Eisenberg <kamrane02@gmail.com>
2026-09-25 16:25:56 -0400

    Task 13 – Add Monopoly package documentation

---
+++ src/edu/towson/cis/cosc442/project1/monopoly/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains the core classes for the Monopoly application,
+ * including the game board, players, cells, cards, and game logic.
+ */
+package edu.towson.cis.cosc442.project1.monopoly;
